da9ebe353568e77e96e85fc815e052eb7cc6dc40
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-10-02 11:33:00 +0300

    Create person.rb

---
+++ person.rb
@@ -0,0 +1,36 @@
+class Person
+    attr_reader :id, :name, :age
+  
+    def initialize(name = 'Unknown', age = nil, parent_permission = true)
+      @id = generate_id
+      @name = name
+      @age = age
+      @parent_permission = parent_permission
+    end
+  
+    def name=(name)
+      @name = name
+    end
+  
+    def age=(age)
+      @age = age
+    end
+  
+    private
+  
+    def of_age?
+      @age >= 18
+    end
+  
+    public
+  
+    def can_use_services?
+      of_age? || @parent_permission
+    end
+  
+    private
+  
+    def generate_id
+      # Implement your ID generation logic here
+    end
+end  
