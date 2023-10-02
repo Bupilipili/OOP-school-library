98770d1cd0c2768665608e947d9cb297e8ddce6a
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-10-02 12:32:30 +0300

    Update student.rb

--- student.rb
+++ student.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(name: 'Unknown', age: nil, parent_permission: true, classroom:)
+  def initialize(classroom:, name: 'Unknown', age: nil, parent_permission: true)
     super(name: name, age: age, parent_permission: parent_permission)
     @classroom = classroom
   end
