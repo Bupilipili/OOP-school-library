8ceab7a8498fe44b1f30684c0ac1d31d620ef536
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-10-02 12:32:19 +0300

    Update person.rb

--- person.rb
+++ person.rb
@@ -1,37 +1,30 @@
 class Person
-    attr_reader :id, :name, :age
-  
-    def initialize(name: 'Unknown', age: nil, parent_permission: true)
-      @id = generate_id
-      @name = name
-      @age = age
-      @parent_permission = parent_permission
-    end
-  
-    def name=(name)
-      @name = name
-    end
-  
-    def age=(age)
-      @age = age
-    end
-  
-    private
-  
-    def of_age?
-      @age >= 18
-    end
-  
-    public
-  
-    def can_use_services?
-      of_age? || @parent_permission
-    end
-  
-    private
-  
-    def generate_id
-      # Implement your ID generation logic here
-    end
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(name: 'Unknown', age: nil, parent_permission: true)
+    @id = generate_id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+
+  public
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  private
+
+  def generate_id
+    # Implement your ID generation logic here
+  end
 end
-  
