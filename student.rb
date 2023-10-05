68963a30e876d680a85dc1cdfd008d639970b912
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-10-05 12:57:50 +0300

    Update student.rb

--- student.rb
+++ student.rb
@@ -2,7 +2,7 @@ class Student < Person
   attr_reader :classroom
 
   def initialize(classroom, name: 'Unknown', age: nil, parent_permission: true)
-    super(name: name, age: age, parent_permission: parent_permission)
+    super(name => name, age => age, parent_permission => parent_permission)
     @classroom = classroom
     classroom.add_student(self)
   end
