c8a9c1c80b2a19b4394461cf1f6e2970aab2bcb2
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-10-09 09:16:55 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
+require_relative 'person'
+require_relative 'student'
+require_relative 'teacher'
+require_relative 'book'
+require_relative 'rental'
+require_relative 'person'
+
 class App
   def initialize
-    # Initialize any required resources here
+    @people = []
+    @books = []
+    @rentals = []
   end
 
   def run
@@ -46,9 +55,9 @@ class App
   def execute_choice(choice)
     case choice
     when 1
-      execute_list_books
+      list_all_books
     when 2
-      execute_list_people
+      list_all_people
     when 3
       execute_create_person
     when 4
@@ -56,11 +65,110 @@ class App
     when 5
       execute_create_rental
     when 6
-      execute_list_rentals_for_person
+      list_rentals_for_person
     else
       puts 'Invalid choice. Please try again.'
     end
   end
 
-  # Define your other methods here for each choice
+  def list_all_books
+    puts 'List of all books:'
+    @books.each_with_index { |book, index| puts "#{index + 1}. #{book.title} by #{book.author}" }
+  end
+
+  def list_all_people
+    puts 'List of all people:'
+    @people.each_with_index { |person, index| puts "#{index + 1}. #{person.name}" }
+  end
+
+  def create_student(classroom, name, age, parent_permission)
+    student = Student.new(classroom, age, name: name, parent_permission: parent_permission)
+    @people << student
+    student
+  end
+
+  def create_teacher(name, age, parent_permission, specialization)
+    teacher = Teacher.new(name, age, parent_permission, specialization)
+    @people << teacher
+    teacher
+  end
+
+  def execute_create_person
+    print 'Enter name: '
+    name = gets.chomp
+
+    print 'Enter age: '
+    age = gets.chomp.to_i
+
+    print 'Enter parent permission (true/false): '
+    parent_permission = gets.chomp.downcase == 'true'
+
+    print 'Enter type (student/teacher): '
+    type = gets.chomp.downcase
+
+    if type == 'student'
+        print 'Enter classroom: '
+      classroom = gets.chomp  
+      student = create_student(classroom, name, age, parent_permission)
+      puts "Student created: #{student.name}"
+    elsif type == 'teacher'
+      print 'Enter specialization: '
+      specialization = gets.chomp
+      teacher = create_teacher(specialization, age, name: name)
+      puts "Teacher created: #{teacher.name}"
+    else
+      puts 'Invalid type. Please try again.'
+    end
+  end
+
+  def execute_create_book
+    print 'Enter title: '
+    title = gets.chomp
+
+    print 'Enter author: '
+    author = gets.chomp
+
+    book = Book.new(title, author)
+    @books << book
+    puts "Book created: #{book.title} by #{book.author}"
+  end
+
+  def execute_create_rental
+    puts 'Select a book:'
+    list_all_books
+    book_index = gets.chomp.to_i - 1
+    return puts 'Invalid book selection.' if book_index < 0 || book_index >= @books.length
+
+    book = @books[book_index]
+
+    puts 'Select a person:'
+    list_all_people
+    person_index = gets.chomp.to_i - 1
+    return puts 'Invalid person selection.' if person_index < 0 || person_index >= @people.length
+
+    person = @people[person_index]
+
+    print 'Enter rental date (format: YYYY-MM-DD): '
+    rental_date = gets.chomp
+
+    rental = Rental.new(book, person, rental_date)
+    @rentals << rental
+    puts "Rental created: #{book.title} rented by #{person.name} on #{rental_date}"
+  end
+
+  def list_rentals_for_person
+    puts 'Select a person:'
+    list_all_people
+    person_index = gets.chomp.to_i - 1
+    return puts 'Invalid person selection.' if person_index < 0 || person_index >= @people.length
+
+    person = @people[person_index]
+
+    puts "Rentals for #{person.name}:"
+    rentals = @rentals.select { |rental| rental.person == person }
+    rentals.each { |rental| puts "#{rental.book.title} rented on #{rental.date}" }
+  end
 end
+
+# Uncomment the following line if you want to run the app directly using `ruby app.rb`
+# App.new.run
